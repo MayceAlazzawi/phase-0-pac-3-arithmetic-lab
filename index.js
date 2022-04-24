594b8cf416841b82ffbc30590732a9788be15fa4
MayceAlazzawi <maynab4@gmail.com>
2022-04-24 21:57:01 +0300

    done

---
+++ index.js
@@ -0,0 +1,43 @@
+function add(a, b) {
+  return a+b
+}
+add()
+
+function subtract(a, b) {
+  return a-b
+}
+subtract()
+
+function multiply(a, b) {
+ return a * b
+}
+multiply()
+
+function divide(a, b) {
+  return a / b;
+}
+divide()
+
+function increment(n) {
+  n += 1;
+  return n
+}
+increment()
+
+function decrement(n) {
+  n -= 1;
+  return n
+}
+decrement()
+
+function makeInt(n) {
+ Number.parseInt('n', 10)
+  return n
+}
+makeInt()
+
+function preserveDecimal(n) {
+  Number.parseFloat(n);
+ return n
+}
+preserveDecimal(2.222)
